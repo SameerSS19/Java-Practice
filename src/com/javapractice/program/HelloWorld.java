b99037382b6e834fc3fa49b62495e83140011a58
unknown <sam1919>
2021-05-11 10:00:04 +0530

    Update

--- src/com/javapractice/program/HelloWorld.java
+++ src/com/javapractice/program/HelloWorld.java
@@ -3,7 +3,8 @@ package com.javapractice.program;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World");
+	System.out.println("Hello world");
+	System.out.println("Local");
 	
 	}
 }
