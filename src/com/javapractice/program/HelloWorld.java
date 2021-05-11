1959414012050d433faf550d33e47616cbe09c0b
sam1919-jpg <61242805+sam1919-jpg@users.noreply.github.com>
2021-05-11 09:12:13 +0530

    update hello Wolrld,java

--- src/com/javapractice/program/HelloWorld.java
+++ src/com/javapractice/program/HelloWorld.java
@@ -3,7 +3,7 @@ package com.javapractice.program;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World");
+	System.out.println("This is a first program Hello World");
 	
 	}
 }
