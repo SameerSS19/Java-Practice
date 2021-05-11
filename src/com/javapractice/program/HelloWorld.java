f23f912d22dc89b57939152745154e2beef27c5e
sam1919-jpg <61242805+sam1919-jpg@users.noreply.github.com>
2021-05-11 10:14:30 +0530

    update

--- src/com/javapractice/program/HelloWorld.java
+++ src/com/javapractice/program/HelloWorld.java
@@ -6,6 +6,8 @@ public class HelloWorld {
 	System.out.println("Hello world");
 	System.out.println("Local");
 	System.out.println("This is a first program Hello World");
+	System.out.println("This is a first Hello World");
+	
 	
 	}
 }
